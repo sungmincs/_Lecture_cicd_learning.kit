b8093f9adf5838fad3379be4307a4bfb1f685234
Hoon Jo <pagaia@hotmail.com>
2024-04-29 12:18:57 +0900

    expand certs from 1 to 10years

--- ch2/2.2/controlplane_node.sh
+++ ch2/2.2/controlplane_node.sh
@@ -36,3 +36,9 @@ find $HOME/_Lecture_cicd_learning.kit -regex ".*\.\(sh\)" -exec chmod 700 {} \;
 EOF
 chmod 700 /usr/local/bin/rerepo-cicd-learning.kit
 
+# extended k8s certifications all
+git clone https://github.com/yuyicai/update-kube-cert.git /tmp/update-kube-cert
+chmod 755 /tmp/update-kube-cert/update-kubeadm-cert.sh
+/tmp/update-kube-cert/update-kubeadm-cert.sh all --cri containerd
+rm -rf /tmp/update-kube-cert
+
