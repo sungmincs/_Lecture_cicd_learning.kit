fffb25e00a973d5a7df09af386344a68a94d859c
Hoon Jo <pagaia@hotmail.com>
2024-05-10 17:00:54 +0900

    chg names

--- ch2/2.3/scripts/copy_launchctl-reload.sh
+++ ch2/2.3/scripts/copy_launchctl-reload.sh
@@ -3,6 +3,7 @@
 # make reload command to local machine 
 cat <<EOF > /usr/local/bin/launchctl-reload
 sudo launchctl unload -w /Library/LaunchDaemons/com.vagrant.vagrant-vmware-utility.plist
+echo "wait for 5seconds..." ; sleep 5 
 sudo launchctl load -w /Library/LaunchDaemons/com.vagrant.vagrant-vmware-utility.plist
 EOF
 
