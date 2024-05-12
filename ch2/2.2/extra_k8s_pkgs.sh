efb2a05a551dedb605c5a0c45e18c5f2af9451c4
Hoon Jo <pagaia@hotmail.com>
2024-05-12 10:58:40 +0900

    upt ingress-ctrl-loadbalancer

--- ch2/2.2/extra_k8s_pkgs.sh
+++ ch2/2.2/extra_k8s_pkgs.sh
@@ -23,7 +23,7 @@ kubectl apply -f $EXTRA_PKGS_ADDR/metallb-native-v0.14.4.yaml
 # config metallb ip range and it cannot deploy now due to CRD cannot create yet 
 (sleep 600 && kubectl apply -f $EXTRA_PKGS_ADDR/metallb-iprange.yaml)&
 
-# nginx ingress ctrl v1.10.1(nodePort) 
+# nginx ingress ctrl v1.10.1(loadbalancer) 
 kubectl apply -f $EXTRA_PKGS_ADDR/ingress-ctrl-loadbalacer-v1.10.1.yaml 
 
 # metrics server v0.7.1 - insecure mode 
