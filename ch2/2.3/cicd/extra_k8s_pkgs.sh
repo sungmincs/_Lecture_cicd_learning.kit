e6c26bc60ba09cae06d49a713f1018e64e54d2d3
Hoon Jo <pagaia@hotmail.com>
2024-05-12 10:48:40 +0900

    upt ingress-ctrl-loadbalancer

--- ch2/2.3/cicd/extra_k8s_pkgs.sh
+++ ch2/2.3/cicd/extra_k8s_pkgs.sh
@@ -24,7 +24,7 @@ kubectl apply -f $EXTRA_PKGS_ADDR/metallb-native-v0.14.4.yaml
 (sleep 600 && kubectl apply -f $EXTRA_PKGS_ADDR/metallb-iprange.yaml)&
 
 # nginx ingress ctrl v1.10.1(nodePort)
-kubectl apply -f $EXTRA_PKGS_ADDR/ingress-ctrl-nodeport-v1.10.1.yaml 
+kubectl apply -f $EXTRA_PKGS_ADDR/ingress-ctrl-loadbalancer-v1.10.1.yaml 
 
 # metrics server v0.7.1 - insecure mode 
 kubectl apply -f $EXTRA_PKGS_ADDR/metrics-server-notls-v0.7.1.yaml
