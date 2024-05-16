f6fa31bf39420d84f93044f8a832d2e29cc5f555
Hoon Jo <pagaia@hotmail.com>
2024-05-16 15:43:30 +0900

    mrg launchctl

--- ch2/2.3/scripts/copy_launchctl-unload-vmware-utility.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# make unload command to local machine 
-cat <<EOF > /usr/local/bin/launchctl-unload-vmware-utility
-sudo launchctl unload -w /Library/LaunchDaemons/com.vagrant.vagrant-vmware-utility.plist
-EOF
-
-sudo chmod 744 /usr/local/bin/launchctl-unload-vmware-utility
