62243998ceeae0cebc16940cc64a35fbdf4870a5
Hoon Jo <pagaia@hotmail.com>
2024-05-17 09:50:39 +0900

    chg permission

--- ch2/2.3/scripts/copy_launchctl-all-vmware-utility.sh
+++ ch2/2.3/scripts/copy_launchctl-all-vmware-utility.sh
@@ -6,16 +6,16 @@ sudo launchctl unload -w /Library/LaunchDaemons/com.vagrant.vagrant-vmware-utili
 echo "wait for 5seconds..." ; sleep 5 
 sudo launchctl load -w /Library/LaunchDaemons/com.vagrant.vagrant-vmware-utility.plist
 EOF
-sudo chmod 770 /usr/local/bin/launchctl-reload
+sudo chmod 775 /usr/local/bin/launchctl-reload
 
 # make load command to local machine
 cat <<EOF > /usr/local/bin/launchctl-load-vmware-utility
 sudo launchctl load -w /Library/LaunchDaemons/com.vagrant.vagrant-vmware-utility.plist
 EOF
-sudo chmod 770 /usr/local/bin/launchctl-load-vmware-utility
+sudo chmod 775 /usr/local/bin/launchctl-load-vmware-utility
 
 # make unload command to local machine
 cat <<EOF > /usr/local/bin/launchctl-unload-vmware-utility
 sudo launchctl unload -w /Library/LaunchDaemons/com.vagrant.vagrant-vmware-utility.plist
 EOF
-sudo chmod 770 /usr/local/bin/launchctl-unload-vmware-utility
+sudo chmod 775 /usr/local/bin/launchctl-unload-vmware-utility
