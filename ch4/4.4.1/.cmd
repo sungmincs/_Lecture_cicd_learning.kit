16be48f590a8473777a05e9f3ded06a76ded3131
sungmin the student <sungmin.infra@gmail.com>
2024-05-24 12:44:45 +0900

    add jenkins install script

--- ch4/4.4.1/.cmd
+++ ch4/4.4.1/.cmd
@@ -1,1 +1,9 @@
 # Install Jenkins
+## install helm script
+./install_jenkins.sh
+
+## update host setting
+```
+192.168.1.11 jenkins.myk8s.local
+```
+### now the Jenkins should be accessible from the host browser with admin/admin
---
+++ ch4/4.4.1/install_jenkins.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+jkopt1="--sessionTimeout=1440"
+jkopt2="--sessionEviction=86400"
+jvopt1="-Duser.timezone=Asia/Seoul"
+jvopt2="-Dcasc.jenkins.config=https://raw.githubusercontent.com/gnu-gnu/5.3.1/main/jenkins-config.yaml"
+jvopt3="-Dhudson.model.DownloadService.noSignatureCheck=true"
+helm install jenkins edu/jenkins \
+--set persistence.enabled=true \
+--set controller.admin.password=admin \
+--set controller.nodeSelector."kubernetes\.io/hostname"=cp-k8s \
+--set controller.tolerations[0].key=node-role.kubernetes.io/control-plane \
+--set controller.tolerations[0].effect=NoSchedule \
+--set controller.tolerations[0].operator=Exists \
+--set controller.runAsUser=1000 \
+--set controller.runAsGroup=1000 \
+--set controller.image.tag="2\.440\.3-jdk17" \
+--set controller.ingress.enabled="true" \
+--set controller.ingress.ingressClassName="nginx" \
+--set controller.ingress.hostName="jenkins.myk8s.local" \
+--set controller.ingress.apiVersion="networking.k8s.io/v1" \
+--set controller.jenkinsOpts="$jkopt1 $jkopt2" \
+--set controller.javaOpts="$jvopt1 $jvopt2 $jvopt3"
