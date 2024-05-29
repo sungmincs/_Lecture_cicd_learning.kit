ccf31eb43824a77979605c1c8e7fb890f3967c1b
Sungmin Lee <sungminl@netflix.com>
2024-05-29 15:58:33 +0900

    try jenkins 2.440.3

--- ch4/4.4.1/install_jenkins.sh
+++ ch4/4.4.1/install_jenkins.sh
@@ -2,7 +2,7 @@
 jkopt1="--sessionTimeout=1440"
 jkopt2="--sessionEviction=86400"
 jvopt1="-Duser.timezone=Asia/Seoul"
-jvopt2="-Dcasc.jenkins.config=https://raw.githubusercontent.com/sungmincs/_Lecture_cicd_learning.kit/sungmin/wip/ch4/4.4.1/jenkins-config.yaml"
+jvopt2="-Dcasc.jenkins.config=https://raw.githubusercontent.com/sungmincs/_Lecture_cicd_learning.kit/sungmin/jenkinstest/ch4/4.4.1/jenkins-config.yaml"
 jvopt3="-Dhudson.model.DownloadService.noSignatureCheck=true"
 helm upgrade --install jenkins edu/jenkins \
 --set controller.admin.password=admin \
@@ -17,7 +17,7 @@ helm upgrade --install jenkins edu/jenkins \
 --set controller.runAsUser=1000 \
 --set controller.runAsGroup=1000 \
 --set controller.serviceType=NodePort \
---set controller.nodePort=40000 \
+--set controller.nodePort=32123 \
 --set controller.tolerations[0].key=node-role.kubernetes.io/control-plane \
 --set controller.tolerations[0].effect=NoSchedule \
 --set controller.tolerations[0].operator=Exists \
