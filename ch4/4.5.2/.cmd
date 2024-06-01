4c30de522e3f073d19a6cd6ffd64f2b776d77736
Sungmin Lee <sungminl@netflix.com>
2024-06-01 16:50:45 +0900

    4.5

---
+++ ch4/4.5.2/.cmd
@@ -0,0 +1,4 @@
+### https://docs.gitlab.com/ee/ci/variables/predefined_variables.html
+### https://docs.gitlab.com/ee/ci/yaml/artifacts_reports.html#artifactsreportsdotenv
+
+### .pre vs .post stage. don't use it unless it's absolutely necessary
