445f7797374f2be6d03c91157695d31973606dc6
Sungmin Lee <sungminl@netflix.com>
2024-06-02 19:49:19 +0900

    5.2.1 github pipeline

---
+++ ch5/5.2.1/.cmd
@@ -0,0 +1,9 @@
+# Setup Secrets
+## Docker login
+
+# Run Actions script
+cp ~/_Lecture_cicd_learning.kit/ch5/5.2.1/1.build-deploy-pipeline.yaml .github/workflows/1.build-deploy-pipeline.yaml
+git add .
+git commit -m "cicd: add build and deploy actions"
+git push origin main
+
