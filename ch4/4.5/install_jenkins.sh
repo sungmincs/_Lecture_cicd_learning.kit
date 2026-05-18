3da62b37aa48f314fe213f1cb082ebe4663e548f
Hoon Jo <pagaia@hotmail.com>
2026-05-18 15:29:55 +0900

    fix(ch4.5): Jenkins 2.541.3→2.555.2 — update-center.json과 버전 일치

--- ch4/4.5/install_jenkins.sh
+++ ch4/4.5/install_jenkins.sh
@@ -6,7 +6,7 @@ jvopt2="-Dcasc.jenkins.config=https://raw.githubusercontent.com/sungmincs/_Lectu
 jvopt3="-Dhudson.model.DownloadService.noSignatureCheck=true"
 helm upgrade --install jenkins edu/jenkins \
 --set controller.admin.password=admin \
---set controller.image.tag="2.541.3-lts-jdk17" \
+--set controller.image.tag="2.555.2-lts-jdk17" \
 --set controller.initContainerEnv[0].name="JENKINS_UC" \
 --set controller.initContainerEnv[0].value="https://raw.githubusercontent.com/sungmincs/_Lecture_cicd_learning.kit/main/ch4/4.5/update-center.json" \
 --set controller.installLatestPlugins=false \
